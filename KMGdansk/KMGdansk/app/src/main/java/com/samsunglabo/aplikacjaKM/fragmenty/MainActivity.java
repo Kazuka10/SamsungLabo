007c7f6c077f857334974f321f6c09e6860b1fed
Michał Kotfica <kotvitz17@gmail.com>
2018-05-17 20:00:59 +0200

    preliminary structure of map module

--- KMGdansk/KMGdansk/app/src/main/java/com/samsunglabo/aplikacjaKM/fragmenty/MainActivity.java
+++ KMGdansk/KMGdansk/app/src/main/java/com/samsunglabo/aplikacjaKM/fragmenty/MainActivity.java
@@ -1,5 +1,6 @@
 package com.samsunglabo.aplikacjaKM.fragmenty;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,9 +8,9 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import net.simplifiedcoding.bottomnavigationexample.R;
+
 //implement the interface OnNavigationItemSelectedListener in your activity class
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +18,10 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         setContentView(R.layout.activity_main);
 
         //loading the default fragment
-        loadFragment(new HomeFragment());
+        Intent map_intent = new Intent(this, MapActivity.class);
+        startActivity(map_intent);
+        //loadFragment(new HomeFragment());
 
-        //getting bottom navigation view and attaching the listener
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
     }
@@ -28,7 +30,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         Fragment fragment = null;
-
         switch (item.getItemId()) {
             case R.id.tab_favorites:
                 fragment = new HomeFragment();
---
+++ KMGdansk/KMGdansk/app/src/main/java/com/samsunglabo/aplikacjaKM/fragmenty/MapActivity.java
@@ -0,0 +1,40 @@
+package com.samsunglabo.aplikacjaKM.fragmenty;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import net.simplifiedcoding.bottomnavigationexample.R;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
+/**
+ * Created by user on 17.05.2018.
+ */
+
+public class MapActivity extends Activity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitvity_map);
+
+        MapView map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        GeoPoint startPoint = new GeoPoint(54.35, 18.63);
+        IMapController mapController = map.getController();
+        mapController.setZoom(10.0);
+        mapController.setCenter(startPoint);
+
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker.setDraggable(true);
+        map.getOverlays().add(startMarker);
+    }
+}
